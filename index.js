dc275cf147ec83189b992407588f4fd284c9222b
bluerai <raiblue@gmx.de>
2025-02-26 12:30:32 +0100

    changed to https

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs-extra';
+
 import { router } from './app/index.js';
 import { logger } from './log.js'
 
-
 const app = express();
+
+const HTTPS_PORT = parseInt(process.env.HTTPS_PORT) || 443;
 
 app.set('view engine', 'pug');
 
@@ -17,6 +21,10 @@ app.use('/app', router);
 
 app.use((request, response) => response.redirect('/app'));
 
-app.listen(process.env.PORT, () => {
-  logger.info('Server is listening to Port ' + process.env.PORT);
+const options = {
+  key: fs.readFileSync(process.env.KEYFILE),
+  cert: fs.readFileSync(process.env.CERTFILE),
+};
+https.createServer(options, app).listen(HTTPS_PORT, () => {
+  logger.info('Https-Server is listening to Port ' + HTTPS_PORT);
 });
